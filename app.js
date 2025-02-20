dcffda14421a135914389052f0d669955bcae741
thong312 <lyminhthong312@gmail.com>
2025-02-20 10:31:01 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const {Client} = require('pg');
+
+const client = new Client({
+    host:"localhost",
+    user:"postgres",
+    port: 5432,
+    password:"12345",
+    database:'postgres',
+});
+
+client.connect();
+
+
+client.query(`select * from public.user`,(err,res)=>{
+  if(!err){
+    console.log(res.rows);
+  }else {
+    console.log(err.message);
+  }
+client.end();
+})
---
+++ database.js
@@ -0,0 +1,13 @@
+// const {createPool} = require('mysql');
+
+// const pool = createPool({
+//     host:"localhost",
+//     user:"root",
+//     password:"rootuser",
+//     connectionLimit:10,
+// });
+// pool.query(`select * from public.user`,(err,res)=>{
+//     return console.log(err,res);    
+// }
+
+// )
